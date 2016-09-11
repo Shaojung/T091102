6c95b408b131e0dca10fd2bdc403cd746fb170ae
Shaojung <shaojung@gmail.com>
2016-09-11 16:09:09 +0800

    Add Button

--- app/src/main/java/tw/com/pcschool/t091102/BFragment.java
+++ app/src/main/java/tw/com/pcschool/t091102/BFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by teacher on 2016/9/11.
@@ -15,8 +17,16 @@ public class BFragment extends Fragment
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_b, container);
+        View v = inflater.inflate(R.layout.fragment_b, container);
+        Button btn = (Button) v.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv3 = (TextView) getActivity().findViewById(R.id.textView3);
+                tv3.setText("Hello Hello");
+            }
+        });
+        return v;
 
     }
 }
