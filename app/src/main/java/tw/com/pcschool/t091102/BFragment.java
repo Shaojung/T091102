e7320e48acc9c9090bd895608cb6bb3a8edb869f
Shaojung <shaojung@gmail.com>
2016-09-11 15:52:00 +0800

    Add Fragment B

---
+++ app/src/main/java/tw/com/pcschool/t091102/BFragment.java
@@ -0,0 +1,22 @@
+package tw.com.pcschool.t091102;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by teacher on 2016/9/11.
+ */
+public class BFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_b, container);
+
+    }
+}
